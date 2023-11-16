fa9bf83678599bcdfa8449a7a99f95bc616d6347
Kentaro Miki <mikiken.dev@gmail.com>
2023-11-16 15:44:33 +0900

    Create dataframe for each type of subject

--- src/background.js
+++ src/background.js
@@ -11,7 +11,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const gradesTable = request.gradesTable;
     console.log(gradesTable);
     let gradesTableCSV = new Blob([gradesTable], { type: "text/csv" });
-    const df = dfd.readCSV(gradesTableCSV);
-    console.log(df);
+    dfd.readCSV(gradesTable.csv).then(df => {
+      let df_basic_liberal_arts = df.query(df['科目大区分'].eq('基礎教養科目').and(df['合否'].eq('合')));
+      let df_integrated_liberal_arts = df.query(df['科目大区分'].eq('総合教養科目').and(df['合否'].eq('合')));
+      let df_advanced_liberal_arts = df.query(df['科目大区分'].eq('高度教養科目').and(df['合否'].eq('合')));
+      let df_foreign_language = df.query(df['科目大区分'].eq('外国語科目').and(df['合否'].eq('合')));
+      let df_information_science = df.query(df['科目大区分'].eq('情報科目').and(df['合否'].eq('合')));
+      let df_health_and_PE = df.query(df['科目大区分'].eq('健康・スポーツ科学').and(df['合否'].eq('合')));
+      let df_others = df.query(df['科目大区分'].eq('その他の科目').and(df['合否'].eq('合')));
+
+      let df_required = df.query(df['区分'].eq('専門科目').and(df['科目大区分'].eq('必修科目').and(df['合否'].eq('合'))));
+      let df_semi_required = df.query(df['区分'].eq('専門科目').and(df['科目大区分'].eq('選択必修科目').and(df['合否'].eq('合'))));
+      let df_elective = df.query(df['区分'].eq('専門科目').and(df['科目大区分'].eq('選択科目').and(df['合否'].eq('合'))));
+
+      console.log(df_integrated_liberal_arts);
+    }).catch(err => {
+      console.log(err);
+    });
   }
 });
